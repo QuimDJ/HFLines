0b209ddfb25c8a3160302e18b4140be1925ab6ee
QuimDJ <quimdalmases@uoc.edu>
2024-04-12 22:10:07 +0200

    .

--- enrollAllOrg.sh
+++ enrollAllOrg.sh
@@ -197,3 +197,83 @@ cp /tmp/hyperledger/org2/peer1/assets/tls-ca/tls-ca-cert.pem /tmp/hyperledger/or
 cp /tmp/hyperledger/org2/admin/msp/signcerts/cert.pem /tmp/hyperledger/org2/msp/admincerts/admin-org2-cert.pem
 cp ./org2-config.yaml /tmp/hyperledger/org2/msp/config.yaml
 echo "Org2 done"
+
+echo 
+echo "Enroll Peer1 (org3)"
+
+# preparation
+mkdir -p /tmp/hyperledger/org3/peer1/assets/ca 
+cp /tmp/hyperledger/org3/ca/admin/msp/cacerts/0-0-0-0-7056.pem /tmp/hyperledger/org3/peer1/assets/ca/org3-ca-cert.pem
+
+mkdir -p /tmp/hyperledger/org3/peer1/assets/tls-ca 
+cp /tmp/hyperledger/tls-ca/admin/msp/cacerts/0-0-0-0-7052.pem /tmp/hyperledger/org3/peer1/assets/tls-ca/tls-ca-cert.pem
+
+# for identity
+export FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/org3/peer1
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/org3/peer1/assets/ca/org3-ca-cert.pem
+export FABRIC_CA_CLIENT_MSPDIR=msp
+
+fabric-ca-client enroll -d -u https://peer1-org3:peer1PW@0.0.0.0:7056
+sleep 5
+
+# for TLS
+export FABRIC_CA_CLIENT_MSPDIR=tls-msp
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/org3/peer1/assets/tls-ca/tls-ca-cert.pem
+
+fabric-ca-client enroll -d -u https://peer1-org3:peer1PW@0.0.0.0:7052 --enrollment.profile tls --csr.hosts peer1-org3 --csr.hosts localhost
+sleep 5
+
+cp /tmp/hyperledger/org3/peer1/tls-msp/keystore/*_sk /tmp/hyperledger/org3/peer1/tls-msp/keystore/key.pem
+
+echo "Enroll Peer2"
+
+# preparation
+mkdir -p /tmp/hyperledger/org3/peer2/assets/ca 
+cp /tmp/hyperledger/org3/ca/admin/msp/cacerts/0-0-0-0-7055.pem /tmp/hyperledger/org3/peer2/assets/ca/org3-ca-cert.pem
+
+mkdir -p /tmp/hyperledger/org3/peer2/assets/tls-ca 
+cp /tmp/hyperledger/tls-ca/admin/msp/cacerts/0-0-0-0-7052.pem /tmp/hyperledger/org3/peer2/assets/tls-ca/tls-ca-cert.pem
+
+# for identity
+export FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/org3/peer2
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/org3/peer2/assets/ca/org3-ca-cert.pem
+export FABRIC_CA_CLIENT_MSPDIR=msp
+
+fabric-ca-client enroll -d -u https://peer2-org3:peer2PW@0.0.0.0:7056
+sleep 5
+
+# for TLS
+export FABRIC_CA_CLIENT_MSPDIR=tls-msp
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/org3/peer2/assets/tls-ca/tls-ca-cert.pem
+
+fabric-ca-client enroll -d -u https://peer2-org3:peer2PW@0.0.0.0:7052 --enrollment.profile tls --csr.hosts peer2-org3 --csr.hosts localhost
+sleep 5
+
+cp /tmp/hyperledger/org3/peer2/tls-msp/keystore/*_sk /tmp/hyperledger/org3/peer2/tls-msp/keystore/key.pem
+
+
+echo "Enroll Admin"
+
+export FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/org3/admin
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/org3/peer1/assets/ca/org3-ca-cert.pem
+export FABRIC_CA_CLIENT_MSPDIR=msp
+
+fabric-ca-client enroll -d -u https://admin-org3:org3AdminPW@0.0.0.0:7056
+
+mkdir -p /tmp/hyperledger/org3/peer1/msp/admincerts
+cp /tmp/hyperledger/org3/admin/msp/signcerts/cert.pem /tmp/hyperledger/org3/peer1/msp/admincerts/org3-admin-cert.pem
+
+mkdir -p /tmp/hyperledger/org3/peer2/msp/admincerts
+cp /tmp/hyperledger/org3/admin/msp/signcerts/cert.pem /tmp/hyperledger/org3/peer2/msp/admincerts/org3-admin-cert.pem
+
+mkdir -p /tmp/hyperledger/org3/admin/msp/admincerts
+cp /tmp/hyperledger/org3/admin/msp/signcerts/cert.pem /tmp/hyperledger/org3/admin/msp/admincerts/org3-admin-cert.pem
+
+mkdir -p /tmp/hyperledger/org3/msp/{admincerts,cacerts,tlscacerts,users}
+cp /tmp/hyperledger/org3/peer1/assets/ca/org3-ca-cert.pem /tmp/hyperledger/org3/msp/cacerts/
+cp /tmp/hyperledger/org3/peer1/assets/tls-ca/tls-ca-cert.pem /tmp/hyperledger/org3/msp/tlscacerts/
+cp /tmp/hyperledger/org3/admin/msp/signcerts/cert.pem /tmp/hyperledger/org3/msp/admincerts/admin-org3-cert.pem
+cp ./org3-config.yaml /tmp/hyperledger/org3/msp/config.yaml
+echo "Org3 done"
+echo 
+
