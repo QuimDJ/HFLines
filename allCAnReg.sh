56e887fd0cf67e5c33ac53c7b9fdf8205c02a764
QuimDJ <quimdalmases@uoc.edu>
2024-04-12 21:28:28 +0200

    m5

--- allCAnReg.sh
+++ allCAnReg.sh
@@ -53,7 +53,7 @@ fabric-ca-client register -d --id.name peer2-org2 --id.secret peer2PW --id.type 
 fabric-ca-client register -d --id.name admin-org2 --id.secret org2AdminPW --id.type admin --id.attrs "abac.init=true:ecert" -u https://0.0.0.0:7055
 fabric-ca-client register -d --id.name user-org2 --id.secret org2UserPW --id.type user -u https://0.0.0.0:7055
 
-echo "-Working on RCA-ORG3-"
+echo "Working on RCA-ORG3"
 
 export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/org2/ca/crypto/ca-cert.pem
 export FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/org3/ca/admin
